ff57276b54e869259581744ef9e5f87beb64c264
Vincent Rivière <vincent.riviere@freesbee.fr>
2017-05-27 22:17:24 +0200

    tmp lftp

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -21,8 +21,11 @@ LOCAL_DIRNAME=release-archives
 REMOTE_DIRNAME=$VERSION
 SSH_USER=vriviere,emutos
 SSH_HOST=frs.sourceforge.net
-SSH_PATH=/home/pfs/p/emutos/travis
+SSH_PATH=/home/pfs/p/emutos/travis2
 
 echo "Deploying $LOCAL_DIRNAME to $SSH_HOST:$SSH_PATH/$REMOTE_DIRNAME"
-set -x
-scp -o StrictHostKeyChecking=no -r release-archives $SSH_USER@$SSH_HOST:$SSH_PATH/$REMOTE_DIRNAME
+echo '$ lftp'
+cat << EOF | tee /dev/stderr | lftp
+open sftp://$SSH_USER:@$SSH_HOST$SSH_PATH
+mirror -R $LOCAL_DIRNAME $REMOTE_DIRNAME
+EOF
